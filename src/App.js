8580e209a12d5bb87b055813a9f7816243edc765
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-03 20:08:40 -0800

    working on lab 27

--- src/App.js
+++ src/App.js
@@ -10,8 +10,41 @@ import Footer from "./components/footer";
 import Form from "./components/form";
 import Results from "./components/results";
 
-// const App () => {
+// const App (props) => {
 
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       data: null,
+  //       requestParams: {},
+  //     };
+  //   }
+  
+  //   callApi = (requestParams) => {
+  //     // mock output
+  //     const data = {
+  //       count: 2,
+  //       results: [
+  //         { name: "fake thing 1", url: "http://fakethings.com/1" },
+  //         { name: "fake thing 2", url: "http://fakethings.com/2" },
+  //       ],
+  //     };
+  //     this.setState({ data, requestParams });
+  //   };
+  
+   
+  //     return (
+  //       <React.Fragment>
+  //         <Header />
+  //         <div>Request Method: {this.state.requestParams.method}</div>
+  //         <div>URL: {this.state.requestParams.url}</div>
+  //         <Form handleApiCall={this.callApi} />
+  //         <Results data={this.state.data} />
+  //         <Footer />
+  //       </React.Fragment>
+  //     );
+
+  // }
 // }
 
 class App extends React.Component {
